b4c73aae3b14e2722d63053b4ecfaa16bd4259ea
Jake Bolewski <jakebolewski@gmail.com>
2013-12-09 21:07:25 -0500

    cleanup oid

--- src/oid.jl
+++ src/oid.jl
@@ -1,5 +1,3 @@
-typealias CGitOid Void
-
 const GIT_OID_RAWSZ = 20
 const GIT_OID_HEXSZ = 40
 
@@ -36,36 +34,27 @@ function raw(oid::Oid)
     copy(oid.oid)
 end
 
+
 Base.show(io::IO, oid::Oid) = begin
     print(io, "Oid(\"$(hex(oid))\")")
 end
 
-Base.string(oid::Oid) = begin
-    
-end
-
-#Base.isequal(oid1::Oid, oid2::Oid) = begin
-#    for i in 1:GIT_OID_RAWSZ
-#        if oid1.oid[i] != oid2.oid[i]
-#            return false
-#        end
-#    end
-#    return true
-#end
 
 Base.isequal(oid1::Oid, oid2::Oid) = begin
     cmp(oid1, oid2) == 0
 end
 
+
 Base.isless(oid1::Oid, oid2::Oid) = begin
     cmp(oid1, oid2) < 0
 end
+
 
 Base.hash(oid::Oid) = begin
     hash(oid.oid)
 end
 
-#TODO: hook this up when ccall is figured out
+
 Base.cmp(oid1::Oid, oid2::Oid) = begin
     git_cmp = ccall((:git_oid_cmp, libgit2),
                     Cint,
