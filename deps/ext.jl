e9fff027d8dd25613f5bc19ee2424f1fac28ada1
Jake Bolewski <jakebolewski@gmail.com>
2013-12-13 16:22:34 -0500

    remove deps as updated api does not need it

--- deps/ext.jl
+++
@@ -1,2 +0,0 @@
-const libwrapgit = find_library(["libwrapgit",],[Pkg.dir("LibGit2", "deps", "usr", "lib"),])
-@assert(libwrapgit != "", "Failed to find required library libwrapgit. Try re-running the package script using Pkg.build(\"LibGit2\")")
