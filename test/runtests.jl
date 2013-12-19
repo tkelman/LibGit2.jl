5c7cd489075491bf1d0eb32d1de61ad57dfb2312
Jake Bolewski <jakebolewski@gmail.com>
2013-12-19 00:34:44 -0500

    remove ref to fix travis

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using LibGit2
 include("util.jl")
 
 testnames = ["oid", "repository", "config", "signature", "index", "object",
-             "reference", "odb"]
+              "odb"]
 
 tests = ARGS==["all"] ? testnames : ARGS
 
