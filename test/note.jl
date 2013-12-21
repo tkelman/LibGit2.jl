e163d77287c91529b612ead6cd0a6c213dab024f
Jake Bolewski <jakebolewski@gmail.com>
2013-12-21 13:05:09 -0500

    add read note from ref test

--- test/note.jl
+++ test/note.jl
@@ -9,6 +9,11 @@
     end
 
     begin # test read note for object from ref
+        id = Oid("36060c58702ed4c2a40832c51758d5344201d89a")
+        obj = lookup(test_repo, id)
+        n = notes(obj, "refs/notes/commits")
+        @test message(n) == "note text\n" 
+        @test oid(n) == Oid("94eca2de348d5f672faf56b0decafa5937e3235e")
     end
 
     begin # test object without note
