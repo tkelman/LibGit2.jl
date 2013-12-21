05151e471656ff6ab22d310700bf0523b3d1c1d5
Jake Bolewski <jakebolewski@gmail.com>
2013-12-21 13:07:45 -0500

    add read note from ref test

--- test/note.jl
+++ test/note.jl
@@ -16,6 +16,18 @@
         @test oid(n) == Oid("94eca2de348d5f672faf56b0decafa5937e3235e")
     end
 
+    begin # test_object_without_note
+        id = "8496071c1b46c854b31185ea97743be6a8774479"
+        obj = lookup(test_repo, id)
+        @test notes(obj) == nothing
+    end
+
+    begin # test_nil_ref_lookup
+        id = Oid("36060c58702ed4c2a40832c51758d5344201d89a")
+        obj = lookup(test_repo, id)
+        @test notes(obj, "refs/notes/missing") == nothing
+    end
+
     begin # test object without note
     end
 
