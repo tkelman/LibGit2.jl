ad8c79c13cc13f190a876eae2b9412054c1e8a3e
Jake Bolewski <jakebolewski@gmail.com>
2014-01-09 03:14:17 -0500

    add another diff symlink test

--- test/diff.jl
+++ test/diff.jl
@@ -598,4 +598,23 @@ end
     @test sum(x -> x.status == :deleted? 1 : 0, ds) == 2
     @test sum(x -> x.status == :modified? 1 : 0, ds) == 0
     @test sum(x -> x.status == :typechange? 1 : 0, ds) == 0
+end
+
+@sandboxed_test "unsymlinked.git" begin
+    a = GitTree(lookup_commit(test_repo, "7fccd7"))
+    b = GitTree(lookup_commit(test_repo, "806999"))
+
+    d = diff(test_repo, a, b, {:include_typechange => true})
+
+    ds = deltas(d)
+    ps = patches(d)
+
+    @test length(d) == 2
+    @test length(ds) == 2
+    @test length(ps) == 2
+    
+    @test sum(x -> x.status == :added? 1 : 0, ds) == 0
+    @test sum(x -> x.status == :deleted? 1 : 0, ds) == 1
+    @test sum(x -> x.status == :modified? 1 : 0, ds) == 0
+    @test sum(x -> x.status == :typechange? 1 : 0, ds) == 1
 end 
