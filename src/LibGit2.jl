5719d0ad8dbd6e404e51b2a736921257f72a5f1e
Jake Bolewski <jakebolewski@gmail.com>
2014-05-15 14:14:54 -0400

    replace our custom init() function with proper module __init__

--- src/LibGit2.jl
+++ src/LibGit2.jl
@@ -4,31 +4,35 @@ include("api.jl")
 
 __threads_handle = nothing
 
-type __GitThreadsHandle
-    function __GitThreadsHandle()
-        h = new()
-        finalizer(h, _ -> api.git_threads_shutdown())
+type __LibGitThreadsHandle
+    inited::Bool
+
+    function __LibGitThreadsHandle()
+        api.git_threads_init()
+        h = new(true)
+        finalizer(h, h -> begin
+            if h.inited
+                api.git_threads_shutdown()
+                h.inited = false
+            end
+        end)
         return h
     end
 end
 
-function init()
+function __init__()
     global __threads_handle
     if __threads_handle == nothing
-        api.git_threads_init()
-	__threads_handle = __GitThreadsHandle()
+	    __threads_handle = __LibGitThreadsHandle()
     end
-    return
 end
 
-init()
-
-include("error.jl")
 include("macros.jl")
+include("error.jl")
 include("oid.jl")
 include("types.jl")
-include("config.jl")
 include("object.jl")
+include("config.jl")
 include("tree.jl")
 include("index.jl")
 include("signature.jl")
