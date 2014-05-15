43ee5343f1b384ae56ee4dc85a0ff3fb187f69f5
Jake Bolewski <jakebolewski@gmail.com>
2014-05-15 12:26:03 -0400

    make Oid immutable, hex is defined in Base so we remove it from exports list.  Sha type should be a bytestring

--- src/oid.jl
+++ src/oid.jl
@@ -1,13 +1,13 @@
-export Oid, hex, oid, raw, iszero
+export Oid, oid, raw, iszero
 
 type Sha1
-    sha1::String
+    sha::ByteString
     
     Sha1(s::String) = begin
         if length(s) != api.OID_HEXSZ
             throw(ArgumentError("invalid sha1 string length"))
         end
-        return new(s)
+        return new(bytestring(s))
     end
 end
 
@@ -15,7 +15,11 @@ macro sha1_str(s)
     Sha1(s)
 end
 
-type Oid
+macro sha_str(s)
+    Sha1(s)
+end
+
+immutable Oid
     oid::Array{Uint8,1}
 
     Oid(b::Array{Uint8, 1}) = begin
@@ -44,12 +48,12 @@ Oid(ptr::Ptr{Uint8}) = begin
 end
 
 oid(id::Oid) = id
-hex(id::Oid) = bytes2hex(id.oid)
 raw(id::Oid) = copy(id.oid)
 
 Base.string(id::Oid) = hex(id)
 Base.show(io::IO, id::Oid) = print(io, "Oid($(string(id)))")
 
+Base.hex(id::Oid)  = bytes2hex(id.oid)
 Base.hash(id::Oid) = hash(hex(id))
 
 Base.cmp(id1::Oid, id2::Oid) = api.git_oid_cmp(pointer(id1.oid), pointer(id2.oid))
