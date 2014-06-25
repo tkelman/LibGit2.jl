998cf5c2267019a5be67f4e3eb6eb6f61728836c
Jake Bolewski <jakebolewski@gmail.com>
2014-06-25 11:40:04 -0400

    remove api reference to libgit in tree.jl

--- src/tree.jl
+++ src/tree.jl
@@ -16,28 +16,23 @@ name(te::GitTreeEntry) = te.name
 Base.isless(te1::GitTreeEntry, te2::GitTreeEntry) = isless(Oid(te1), Oid(te2))
 
 Base.length(t::GitTree) = 
-    int(ccall((:git_tree_entrycount, api.libgit2), Csize_t, (Ptr{Void},), t))
+    int(ccall((:git_tree_entrycount, :libgit2), Csize_t, (Ptr{Void},), t))
                                
 Base.filemode(te::GitTreeEntry) = te.filemode
 
 let 
     function tree_entry_name(ptr::Ptr{Void})
-        nptr = ccall((:git_tree_entry_name, api.libgit2), Ptr{Uint8},
-                     (Ptr{Void},), ptr)
-        @assert nptr != C_NULL
+        nptr = ccall((:git_tree_entry_name, :libgit2), Ptr{Uint8}, (Ptr{Void},), ptr)
         return bytestring(nptr)
     end
 
     function tree_entry_oid(ptr::Ptr{Void})
-        idptr = ccall((:git_tree_entry_id, api.libgit2), Ptr{Uint8}, 
-                      (Ptr{Void},), ptr)
-        @assert idptr != C_NULL
+        idptr = ccall((:git_tree_entry_id, :libgit2), Ptr{Uint8}, (Ptr{Void},), ptr)
         return Oid(idptr)
     end
 
     function tree_entry_type(ptr::Ptr{Void})
-        t = ccall((:git_tree_entry_type, api.libgit2), Cint, 
-                  (Ptr{Void},), ptr)
+        t = ccall((:git_tree_entry_type, :libgit2), Cint, (Ptr{Void},), ptr)
         t == api.OBJ_BLOB   && return GitBlob
         t == api.OBJ_COMMIT && return GitCommit
         t == api.OBJ_TAG    && return GitTag
@@ -46,12 +41,10 @@ let
     end
 
     function tree_entry_filemode(ptr::Ptr{Void})
-        ccall((:git_tree_entry_filemode, api.libgit2), Cint,
-              (Ptr{Void},), ptr)
+        ccall((:git_tree_entry_filemode, :libgit2), Cint, (Ptr{Void},), ptr)
     end
 
     #TODO: ownership of tree entry pointer 
-
     function GitTreeEntry(ptr::Ptr{Void}, owns::Bool=false)
         @assert ptr != C_NULL
         ty   = tree_entry_type(ptr)
@@ -63,7 +56,7 @@ let
 end
 
 function entry_byname(t::GitTree, filename::String)
-    eptr = ccall((:git_tree_entry_byname, api.libgit2), Ptr{Void},
+    eptr = ccall((:git_tree_entry_byname, :libgit2), Ptr{Void},
                  (Ptr{Void}, Ptr{Uint8}), t, filename)
     if eptr == C_NULL
         return nothing
@@ -73,13 +66,13 @@ end
 
 function entry_bypath(t::GitTree, path::String)
     eptr = Ptr{Void}[0]
-    @check ccall((:git_tree_entry_bypath, api.libgit2), Cint,
+    @check ccall((:git_tree_entry_bypath, :libgit2), Cint,
                   (Ptr{Ptr{Void}}, Ptr{Uint8}), eptr, path)
     return GitTreeEntry(eptr[1], true)
 end
 
 function entry_byindex(t::GitTree, idx::Integer)
-    eptr = ccall((:git_tree_entry_byindex, api.libgit2), Ptr{Void},
+    eptr = ccall((:git_tree_entry_byindex, :libgit2), Ptr{Void},
                  (Ptr{Void}, Csize_t), t, idx - 1) 
     if eptr == C_NULL
         return nothing
@@ -88,7 +81,7 @@ function entry_byindex(t::GitTree, idx::Integer)
 end
 
 function entry_byid(t::GitTree, id::Oid)
-    eptr = ccall((:git_tree_entry_byid, api.libgit2), Ptr{Void},
+    eptr = ccall((:git_tree_entry_byid, :libgit2), Ptr{Void},
                  (Ptr{Void}, Ptr{Oid}), t, &id)
     if eptr == C_NULL
         return nothing
@@ -145,7 +138,7 @@ function walk(t::GitTree, order=:postorder)
     else
         throw(ArgumentError("walk order can be :preorder or :postorder, got :$order"))
     end
-    @task ccall((:git_tree_walk, api.libgit2), Cint,
+    @task ccall((:git_tree_walk, :libgit2), Cint,
                 (Ptr{Void}, Cint, Ptr{Void}, Ptr{Void}), 
                 t, mode, c_cb_treewalk, C_NULL)
 end
@@ -189,14 +182,14 @@ end
 
 GitTreeBuilder(r::GitRepo) = begin
     tbptr = Ptr{Void}[0] 
-    @check ccall((:git_treebuilder_create, api.libgit2), Cint,
+    @check ccall((:git_treebuilder_create, :libgit2), Cint,
                  (Ptr{Ptr{Void}}, Ptr{Void}), tbptr, C_NULL)
     return GitTreeBuilder(tbptr[1], r)
 end 
 
 free!(tb::GitTreeBuilder) = begin
     if tb.ptr != C_NULL
-        ccall((:git_treebuilder_free, api.libgit2), Void, (Ptr{Void},), tb.ptr)
+        ccall((:git_treebuilder_free, :libgit2), Void, (Ptr{Void},), tb.ptr)
         tb.ptr = C_NULL
     end
 end
@@ -204,7 +197,7 @@ end
 Base.convert(::Type{Ptr{Void}}, tb::GitTreeBuilder) = tb.ptr
 
 Base.insert!(tb::GitTreeBuilder, filename::String, id::Oid, filemode::Int) = begin
-    @check ccall((:git_treebuilder_insert, api.libgit2), Cint,
+    @check ccall((:git_treebuilder_insert, :libgit2), Cint,
                  (Ptr{Ptr{Void}}, Ptr{Void}, Ptr{Uint8}, Ptr{Oid}, Cint),
                  C_NULL, tb, filename, &id, filemode)
     return tb
@@ -212,7 +205,7 @@ end
 
 function write!(tb::GitTreeBuilder)
     id = Oid()
-    @check ccall((:git_treebuilder_write, api.libgit2), Cint,
+    @check ccall((:git_treebuilder_write, :libgit2), Cint,
                  (Ptr{Oid}, Ptr{Void}, Ptr{Void}), &id, tb.repo, tb) 
     return id
 end
