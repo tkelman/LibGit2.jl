b89b7da9cdebb6c82e96c21c99f22fc71f282d19
Jake Bolewski <jakebolewski@gmail.com>
2014-07-01 16:14:20 -0400

    refactor odb.jl

--- src/odb.jl
+++ src/odb.jl
@@ -1,6 +1,7 @@
 export Odb, OdbObject, OdbWrite, OdbRead,
        exists, data, open_wstream, read_header
 
+#TODO: rename to GitOdb
 type Odb
     ptr::Ptr{Void}
 
@@ -14,26 +15,22 @@ end
 
 free!(o::Odb) = begin 
     if o.ptr != C_NULL 
-        api.git_odb_free(o.ptr)
+        ccall((:git_odb_free, :libgit2), Void, (Ptr{Void},), o.ptr)
         o.ptr = C_NULL
     end
 end
 
-function exists(o::Odb, id::Oid)
-    @check o.ptr != C_NULL
-    res = api.git_odb_exists(o.ptr, id.oid)
-    return bool(res)
-end
+Base.convert(::Type{Ptr{Void}}, o::Odb) = o.ptr 
 
-Base.in(id::Oid, o::Odb) = begin
-    exists(o, id)
-end
+exists(o::Odb, id::Oid)  = bool(ccall((:git_odb_exists, :libgit2), Cint, (Ptr{Void}, Ptr{Oid}), o, &id))
+Base.in(id::Oid, o::Odb) = exists(o, id)
 
 function read_header(odb::Odb, id::Oid)
     nbytes = Csize_t[0]
     otype  = Cint[0]
-    @check api.git_odb_read_header(nbytes, otype, odb.ptr, id.oid)
-    return {:type   => gitobj_const_type(otype[1]),
+    @check ccall((:git_odb_read_header, :libgit2), Cint,
+                 (Ptr{Csize_t}, Ptr{Cint}, Ptr{Void}, Ptr{Oid}), nbytes, otype, odb, &id)
+    return {:type => gitobj_const_type(otype[1]),
             :nbytes => nbytes[1]}
 end
 
@@ -43,7 +40,7 @@ end
 
 OdbObject(ptr::Ptr{Void}) = begin
     @assert ptr != C_NULL
-    obj_type = api.git_odb_object_type(ptr)
+    obj_type = ccall((:git_odb_object_type, :libgit2), Cint, (Ptr{Void},), ptr)
     T = gitobj_const_type(obj_type)
     o = OdbObject{T}(ptr)
     finalizer(o, free!)
@@ -52,27 +49,17 @@ end
 
 free!(o::OdbObject) = begin
     if o.ptr != C_NULL
-        api.git_odb_object_free(o.ptr)
+        ccall((:git_odb_object_free, :libgit2), Void, (Ptr{Void},), o.ptr)
         o.ptr = C_NULL
     end
 end
 
-Base.length(o::OdbObject) = begin
-    @assert o.ptr != C_NULL
-    return div(sizeof(o), sizeof(Cchar))
-end
-
-Base.sizeof(o::OdbObject) = begin
-    @assert o.ptr != C_NULL
-    return int(api.git_odb_object_size(o.ptr))
-end
+Base.sizeof(o::OdbObject) = int(ccall((:git_odb_object_size, :libgit2), Csize_t, (Ptr{Void},), o))
+Base.length(o::OdbObject) = div(sizeof(o), sizeof(Uint8))
 
 function data(o::OdbObject)
-    @assert o.ptr != C_NULL
-    data_ptr::Ptr{Cchar} = api.git_odb_object_data(o.ptr)
-    if data_ptr == C_NULL
-        error("odb object data pointer is NULL")
-    end
+    data_ptr = ccall((:git_odb_object_data, :libgit2), Ptr{Uint8}, (Ptr{Void},), o)
+    @assert data_ptr != C_NULL
     return bytestring(data_ptr)
 end
 
@@ -80,71 +67,73 @@ abstract OdbIO
 
 free!(os::OdbIO) = begin
     if os.ptr != C_NULL
-        #TODO: close before gc?
-        api.git_odb_stream_free(os.ptr)
+        #TODO: close before gc? 
+        ccall((:git_odb_stream_free, :libgit2), Void, (Ptr{Void},), os.ptr) 
         os.ptr = C_NULL
     end
 end
 
 type OdbWrite <: OdbIO
     ptr::Ptr{Void}
-    oid::Oid
+    id::Oid
 
     function OdbWrite(ptr::Ptr{Void})
         @assert ptr != C_NULL
-        oid_ptr = api.git_odb_object_id(ptr)
-        if oid_ptr == C_NULL
-            error("oid pointer for write stream is NULL")
-        end
+        oid_ptr = ccall((:git_odb_object_id, :libgit2), Ptr{Oid}, (Ptr{Void},), ptr)
+        @assert oid_ptr != C_NULL
         s = new(ptr, Oid(oid_ptr))
         finalizer(s, free!)
         return s
     end
 end
 
-Oid(odbw::OdbWrite) = odbw.oid
+Base.convert(::Type{Ptr{Void}}, o::OdbWrite) = o.ptr 
+
+Oid(odbw::OdbWrite) = odbw.id
 
 function open_wstream{T<:GitObject}(::Type{T}, odb::Odb, len::Int)
     @assert odb.ptr != C_NULL
     @assert len > 0
     gtype = git_otype(T)
-    clen = convert(Csize_t, len)
-    stream_ptr = Array(Ptr{Void}, 1)
-    @check api.git_odb_open_wstream(stream_ptr, odb.ptr, clen, gtype)
+    stream_ptr = Ptr{Void}[0] 
+    @check ccall((:git_odb_open_wstream, :libgit2), Cint, 
+                 (Ptr{Ptr{Void}}, Ptr{Void}, Csize_t, Cint), stream_ptr, odb, clen, gtype)
     return OdbWrite(stream_ptr[1])
 end
 
-Base.isreadable(io::OdbWrite) = false 
+Base.isreadable(io::OdbWrite)  = false 
 Base.iswriteable(io::OdbWrite) = true
 
-Base.write(io::OdbWrite, b::ByteString) = begin 
-    @assert io.ptr != C_NULL
+#TODO: this needs to be reworked
+Base.write(io::OdbWrite, buffer::ByteString) = begin 
     len = length(b)
-    @check api.git_odb_stream_write(io.ptr, b, len)
-    return len
+    @check ccall((:git_odb_stream_write, :libgit2), Cint, 
+                 (Ptr{Void}, Ptr{Uint8}, Csize_t), io, buffer, len) 
+    return io
 end
 
 #TODO: this is broken...
-Base.write{T}(io::OdbWrite, b::Array{T}) = begin
-    @assert io.ptr != C_NULL
+Base.write{T}(io::OdbWrite, buffer::Array{T}) = begin
     @assert isbits(T)
     ptr = convert(Ptr{Uint8}, b)
     len = convert(Csize_t, div(length(b) * sizeof(T), sizeof(Uint8)))
-    @check api.git_odb_stream_write(io.ptr, b, len)
+    @check ccall((:git_odb_stream_write, :libgit2), Cint, 
+                 (Ptr{Void}, Ptr{Uint8}, Csize_t), io, buffer, len)
     return len
 end
 
-Base.close(os::OdbWrite) = begin
-    @assert os.ptr != C_NULL
-    @check api.git_odb_stream_finalize_write(Oid(os).oid, os.ptr)
-    return nothing
+Base.close(io::OdbWrite) = begin
+    ioid = Oid(io)
+    @check ccall((:git_odb_stream_finalize_write, :libgit2), Cint, (Ptr{Oid}, Ptr{Void}), &ioid, io)
+    return io 
 end
 
 type OdbRead <: OdbIO
     ptr::Ptr{Void}
 end
 
-Base.isreadable(io::OdbRead) = true
+Base.convert(::Type{Ptr{Void}}, o::OdbRead) = o.ptr 
+Base.isreadable(io::OdbRead)  = true
 Base.iswriteable(io::OdbRead) = false
 
 #TODO: this is broken ...
@@ -154,10 +143,11 @@ Base.iswriteable(io::OdbRead) = false
 #    resize!(b, nr)
 #end
  
-Base.readbytes!(io::OdbRead, b::Vector{Uint8}, nb=length(b)) = begin
+Base.readbytes!(io::OdbRead, buffer::Vector{Uint8}, nb=length(b)) = begin
     @assert io.ptr != C_NULL
     len = convert(Csize_t, length(b))
-    ret = @check api.git_odb_stream_read(io.ptr, b, len)
+    ret = @check ccall((:git_odb_stream_read, :libgit2), Cint,
+                       (Ptr{Void}, Ptr{Uint8}, Csize_t), io, buffer, len)
     @assert len > 0
     return len
 end
