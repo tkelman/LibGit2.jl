f53e0086175a6aedf98d503b437b1fd132fad40d
Jake Bolewski <jakebolewski@gmail.com>
2014-07-03 01:00:26 -0400

    refactor commit, follow 1 based indexing for commit parents

--- src/commit.jl
+++ src/commit.jl
@@ -3,15 +3,12 @@ export git_otype, message, tree, tree_id,
 
 function message(c::GitCommit, raw::Bool=false)
     local msg_ptr::Ptr{Uint8}
-    if raw
-        msg_ptr = ccall((:git_commit_message_raw, :libgit2), Ptr{Uint8}, (Ptr{Void},), c)
-    else
-        msg_ptr = ccall((:git_commit_message, :libgit2), Ptr{Uint8}, (Ptr{Void},), c)
-    end
+    msg_ptr = raw? ccall((:git_commit_message_raw, :libgit2), Ptr{Uint8}, (Ptr{Void},), c) :
+                   ccall((:git_commit_message, :libgit2), Ptr{Uint8}, (Ptr{Void},), c)
     if msg_ptr == C_NULL 
         return nothing
     end
-    return bytestring(msg_ptr)
+    return utf8(bytestring(msg_ptr))
 end
 
 function GitTree(c::GitCommit)
@@ -20,8 +17,7 @@ function GitTree(c::GitCommit)
     return GitTree(tree_ptr[1])
 end
 
-git_tree_id(c::GitCommit) = Oid(ccall((:git_commit_tree_id, :libgit2), Ptr{Uint8}, 
-                                      (Ptr{Void},), c))
+git_tree_id(c::GitCommit) = Oid(ccall((:git_commit_tree_id, :libgit2), Ptr{Uint8}, (Ptr{Void},), c))
 
 function author(c::GitCommit)
     ptr = ccall((:git_commit_author, :libgit2), Ptr{SignatureStruct}, (Ptr{Void},), c)
@@ -36,25 +32,21 @@ function committer(c::GitCommit)
 end
 
 function parent(c::GitCommit, n::Integer)
-    n >= 0 || throw(ArgumentError("n must be greater than or equal to 0"))
+    n >= 1 || throw(ArgumentError("n must be greater than or equal to 1"))
     commit_ptr = Ptr{Void}[0]
-    @check ccall((:git_commit_parent, :libgit2), Cint, (Ptr{Ptr{Void}}, Ptr{Void}, Cuint), commit_ptr, c, n) 
+    @check ccall((:git_commit_parent, :libgit2), Cint,
+                 (Ptr{Ptr{Void}}, Ptr{Void}, Cuint), commit_ptr, c, n-1)
     return GitCommit(commit_ptr[1])
 end
 
+parent_count(c::GitCommit) = int(ccall((:git_commit_parentcount, :libgit2), Csize_t, (Ptr{Void},), c))
 function parents(c::GitCommit)
     n = parent_count(c)
-    ps = GitCommit[]
-    for i in 0:n-1
-        push!(ps, parent(c, i))
-    end
-    return ps
+    return GitCommit[parent(c, i) for i=1:n]
 end
 
 function parent_id(c::GitCommit, n::Integer)
-    n >= 0 || throw(ArgumentError("n must be greater than or equal to 0"))
-    oid_ptr = ccall((:git_commit_parent_id, :libgit2), Ptr{Uint8}, (Ptr{Void}, Cuint), c, n)
+    n >= 1 || throw(ArgumentError("n must be greater than or equal to 1"))
+    oid_ptr = ccall((:git_commit_parent_id, :libgit2), Ptr{Uint8}, (Ptr{Void}, Cuint), c, n-1)
     return Oid(oid_ptr)
 end
-
-parent_count(c::GitCommit) = int(ccall((:git_commit_parentcount, :libgit2), Csize_t, (Ptr{Void},), c))
